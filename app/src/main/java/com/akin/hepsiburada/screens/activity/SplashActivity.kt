83bd0fa868e6803f06384caaeb244550e3edd005
Akin-junior <keskinbas13@gmail.com>
2021-10-19 00:31:38 +0300

    created road map

---
+++ app/src/main/java/com/akin/hepsiburada/screens/activity/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.akin.hepsiburada.screens.activity
+
+import android.animation.Animator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
+import com.akin.hepsiburada.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import java.lang.Exception
+
+class SplashActivity : AppCompatActivity() {
+
+    private val animation by lazy { findViewById<LottieAnimationView>(R.id.animationView) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+        animation.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                println("basladi")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+
+                //Navigate to onboarding fragment
+                println("bitti")
+
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+
+            }
+        })
+
+    }
+}
---
+++ app/src/main/java/com/akin/hepsiburada/screens/fragments/DetailFragment.kt
@@ -0,0 +1,37 @@
+package com.akin.hepsiburada.screens.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akin.hepsiburada.R
+import com.akin.hepsiburada.screens.activity.SplashActivity
+
+class DetailFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+}
