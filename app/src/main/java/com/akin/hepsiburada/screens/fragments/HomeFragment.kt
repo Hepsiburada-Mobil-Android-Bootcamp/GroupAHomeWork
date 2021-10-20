f87c2bedafcc53a1ad076779b9c80e6c1700ab69
Akin-junior <keskinbas13@gmail.com>
2021-10-20 18:50:45 +0300

    Firebase init hatalari cozuldu Model classlari ve ViewModeller olusturuldu

--- app/src/main/java/com/akin/hepsiburada/screens/fragments/HomeFragment.kt
+++ app/src/main/java/com/akin/hepsiburada/screens/fragments/HomeFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.akin.hepsiburada.R
-
+import androidx.fragment.app.viewModels
+import com.akin.hepsiburada.databinding.FragmentHomeBinding
+import com.akin.hepsiburada.domain.FirebaseViewModel
 
 
 class HomeFragment : Fragment() {
@@ -17,17 +18,43 @@ class HomeFragment : Fragment() {
 
     }
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel : FirebaseViewModel by viewModels ()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        viewModel.foodList.observe(viewLifecycleOwner, {
+            println(it[0].name)
+
+        })
+        viewModel.categoriesList.observe(viewLifecycleOwner, {
+            println(it.toString())
+
+        })
+
+
+
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
