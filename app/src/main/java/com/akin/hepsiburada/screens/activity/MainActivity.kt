de9de1a050dbcfdcb8465a032d9997c940eba77e
Akin-junior <keskinbas13@gmail.com>
2021-10-23 12:47:11 +0300

    loading progress.xml add glide convert single use type add food func improvements(check if image null) blank page filled dummy datas drawer improvements

--- app/src/main/java/com/akin/hepsiburada/screens/activity/MainActivity.kt
+++ app/src/main/java/com/akin/hepsiburada/screens/activity/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.core.view.MenuItemCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.akin.hepsiburada.R
@@ -34,38 +35,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var profilePic: ImageView
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       // bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        // fab button logic
         fabButton = binding.fab
-
         fabButton.setOnClickListener {
             val bottomFragment = BottomSheetFragment()
             bottomFragment.setStyle(
                 DialogFragment.STYLE_NORMAL,
-                R.style.ThemeOverlay_Demo_BottomSheetDialog)
-            bottomFragment.show(supportFragmentManager,"TAG")
+                R.style.ThemeOverlay_Demo_BottomSheetDialog
+            )
+            bottomFragment.show(supportFragmentManager, "TAG")
 
         }
 
 
         //nav logic
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-
         navController = navHostFragment.navController
 
         //bottom nav logic
         val bottomNav = binding.bottomNavigationView
         bottomNav.background = null
         bottomNav.menu.getItem(2).isEnabled = false
-
         bottomNav.setupWithNavController(navController)
 
 
@@ -76,39 +72,36 @@ class MainActivity : AppCompatActivity() {
         val drawerNav = binding.navigationView
         drawerNav.itemIconTintList = null
         val drawerIcon = binding.drawerMenuIcon
-
         drawerIcon.setOnClickListener {
             drawer.openDrawer(GravityCompat.START)
         }
         NavigationUI.setupWithNavController(drawerNav, navController)
-        val menuItem: MenuItem = drawerNav.getMenu()
-            .findItem(R.id.changeTheme) // This is the menu item that contains your switch
 
-        val drawer_switch = menuItem.getActionView().findViewById(R.id.drawer_switch) as SwitchCompat
+
+        // This is the menu item that contains your switch
+        val menuItem: MenuItem = drawerNav.menu
+            .findItem(R.id.changeTheme)
+
+        //switch and light dark mode
+        val drawer_switch =
+            menuItem.actionView.findViewById(R.id.drawer_switch) as SwitchCompat
         drawer_switch.setOnCheckedChangeListener { compoundButton, b ->
-            if (b){
+            if (b) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }else{
+            } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
 
         }
-        //fab logic
-        drawerNav.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.exit -> println("sada")
-
-
-            }
-            true
+        val exitButton : MenuItem = drawerNav.menu.findItem(R.id.exit)
+        exitButton.setOnMenuItemClickListener {
+            println("asdsa")
+            //Parseuser logout
+            return@setOnMenuItemClickListener true
         }
 
 
-
-
     }
-
-
 
 
 }
